c6887a00d81c9c236dd821443783ecbd13a4a436
Garima Gupta <ggupta8@illinois.edu>
2016-11-08 16:21:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/garima/bubblr/HomeScreen.java
@@ -0,0 +1,29 @@
+package com.example.garima.bubblr;
+
+import android.app.ActionBar;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        {
+            //android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+            //actionBar.hide();
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getActionBar().setDisplayHomeAsUpEnabled(true); // also required
+        }
+    }
+}
